7699a8416134d1df47429adf6af52a723057c098
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2022-07-06 15:19:52 +0100

    Initial Commit

---
+++ fitness-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  fitness-app
+//
+//  Created by Selvin Selbaraju on 06/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ fitness-app/fitness_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  fitness_appApp.swift
+//  fitness-app
+//
+//  Created by Selvin Selbaraju on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct fitness_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
